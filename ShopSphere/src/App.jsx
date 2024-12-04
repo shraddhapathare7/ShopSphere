695e6471c253fc05b012793bfe6f6b8305349f13
shraddhapathare7 <shraddhapathare137@gmail.com>
2024-12-04 13:11:45 +0530

    Completed with design implementation

--- ShopSphere/src/App.jsx
+++ ShopSphere/src/App.jsx
@@ -1,17 +1,23 @@
-import './App.css'
-import AOS from 'aos'
-import "aos/dist/aos.css"
-import { useEffect, useState } from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
+import Popup from "./components/Popup/Popup";
 
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
 
-function App() {
-  const [orderPopup, setOrderPopup] = useState(false);
-
-  const handleOrderPopup = () =>{
+  const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
   };
-
-  useEffect(() =>{
+  React.useEffect(() => {
     AOS.init({
       offset: 100,
       duration: 800,
@@ -19,14 +25,21 @@ function App() {
       delay: 100,
     });
     AOS.refresh();
-  })
+  }, []);
+
   return (
-    <>
-      <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
 
-      </div>
-    </>
-  )
-}
-
-export default App
+export default App;
